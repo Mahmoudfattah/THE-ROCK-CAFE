5d25aaab807815e2b4ec3f43c8d045252e672a3c
Mahmoud-Fattah <hoodafatah@gmail.com>
2026-04-25 01:58:44 +0300

    fixed the captial issue

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import { LangProvider } from "./context/context.jsx";
+import { LangProvider } from "./context/Context.jsx";
 import "./index.css";
 import App from "./App.jsx";
 
